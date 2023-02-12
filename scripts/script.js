1a53ad01e5c5d9370026f3971f5a9a86eae022a2
Pavel Shershnev <pavelsersnev@gmail.com>
2023-02-13 00:16:12 +1200

    v7

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,3 @@
-// let popup = document.querySelector(".popup");
-// let redact = document.querySelectorAll(".redact");
-// let popupName = document.querySelector(".popup__name");
-// let popupAchievements = document.querySelector(".popup__achievements");
-
-// for (let index = 0; index < redact.length; index++) {
-//    button = redact[index];
-//    button.addEventListener("click", function (e) {
-//       popup.classList.toggle("active");
-//    });
-// };
-
-// let popupButton = document.querySelector(".popup__save");
-
-// popupButton.addEventListener("click", function (e) {
-//    e.preventDefault();
-//    let profileName = document.querySelector(".profile__title");
-//    let profileAchievements = document.querySelector(".profile__subtitle");
-//    profileName.textContent = popupName.value;
-//    profileAchievements.textContent = popupAchievements.value;
-//    popup.classList.remove("active");
-// });
-
-
-
 let popup = document.querySelector(".popup");
 let formElement = document.querySelector(".form")
 let nameInput = document.querySelector(".form__profile-name");
