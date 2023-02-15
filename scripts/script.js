6fb6afa450a3432ffb78c0f4c85bd3e00eb8a529
Pavel Shershnev <pavelsersnev@gmail.com>
2023-02-15 18:13:16 +1200

    v7

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 let popup = document.querySelector(".popup");
 let formElement = document.querySelector(".form")
-let nameInput = document.querySelector(".form__profile-name");
-let jobInput = document.querySelector(".form__profile-job");
+let nameInput = document.querySelector(".form__input_type_name");
+let jobInput = document.querySelector(".form__input_type_job");
 let profileName = document.querySelector(".profile__title");
 let profileJob = document.querySelector(".profile__subtitle");
 let openPopup = document.querySelector(".profile__edit-button");
@@ -14,15 +14,13 @@ function handleFormSubmit(evt) {
    popupClose();
 };
 
-function popupOpen(e) {
-   if (openPopup) {
-      popup.classList.add("popup__opened");
-   }
+function popupOpen() {
+   nameInput.value = profileName.textContent;
+   jobInput.value = profileJob.textContent;
+   popup.classList.add("popup_opened");
 };
-function popupClose(e) {
-   if (closePopup) {
-      popup.classList.remove("popup__opened");
-   }
+function popupClose() {
+   popup.classList.remove("popup_opened");
 };
 
 openPopup.addEventListener("click", popupOpen);
