31fe1eb3b2147ea47dc3b2df393e5f06a09faf69
Pavel Shershnev <pavelsersnev@gmail.com>
2023-02-23 08:59:13 +1200

    s5v2

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,56 @@ const openPopupButtonProfile = document.querySelector(".profile__edit-button");
 const addCardButton = document.querySelector(".profile__add-button");
 const popupCard = document.querySelector(".popup_type_card");
 const closePopupButton = document.querySelectorAll(".popup__close");
+const template = document.querySelector("#card").content;
+const cardsBlock = document.querySelector(".cards");
 const like = document.querySelectorAll(".card__like");
+const initialCards = [
+   {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+   },
+   {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+   },
+   {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+   },
+   {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+   },
+   {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+   },
+   {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+   }
+];
+const formCard = document.querySelector(".form_card");
+function create(name, link) {
+   const newCard = template.cloneNode(true);
+   newCard.querySelector(".card__img").src = link;
+   newCard.querySelector(".card__title").textContent = name;   
+   newCard.querySelectorAll(".card__like").forEach(item => {
+      item.addEventListener("click", () => {
+         item.classList.toggle("card__like_active")
+      });
+   })
+   cardsBlock.prepend(newCard);
+   return newCard;
+};
+
+formCard.addEventListener("submit", (evt) => {
+   evt.preventDefault();
+   
+   closePopup(popupCard);
+});
+
+initialCards.forEach (item => {create(item.name, item.link)});
 
 function handleFormSubmit(evt) {
    evt.preventDefault();
@@ -41,47 +90,23 @@ addCardButton.addEventListener("click", function () {
    openPopup(popupCard);
 });
 formElement.addEventListener('submit', handleFormSubmit);
-like.forEach(item => {
-   item.addEventListener("click", function () {
-      item.classList.toggle("card__like_active")
-   });
-});
-
-const initialCards = [
-   {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-   },
-   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-   },
-   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-   },
-   // {
-   //    name: 'Камчатка',
-   //    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-   // },
-   // {
-   //    name: 'Холмогорский район',
-   //    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-   // },
-   // {
-   //    name: 'Байкал',
-   //    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-   // }
-];
-const template = document.querySelector("#card").content;
-const cloneTemplate = template.querySelector(".card").cloneNode(true);
-const cardsBlock = document.querySelector(".cards");
-
-for (let i = 0; i <= initialCards.length; i++) {
-   cloneTemplate.querySelector(".card__title").textContent = initialCards[i].name;
-   cloneTemplate.querySelector(".card__img").setAttribute("src", initialCards[i].link);
-   cardsBlock.append(cloneTemplate);
-}
 
 
 
+
+// let newObj = initialCards.map( item => {
+//    return {name: item.name, link: item.link};
+// });
+
+// function enterCard() {
+//    newObj.forEach(createCard);
+// };
+
+// function createCard({name, link}) {
+//    const cloneTemplate = template.querySelector(".card").cloneNode(true);
+//    cloneTemplate.querySelector(".card__title").textContent = name;
+//    cloneTemplate.querySelector(".card__img").src = link;
+//    cardsBlock.prepend(cloneTemplate);
+// };
+
+// enterCard();
