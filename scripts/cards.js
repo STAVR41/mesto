1e043653845e1c84ea6e308225d0226a47470e99
Pasha2 <pavelsersnev@gmail.com>
2023-02-25 00:27:56 +1000

    s5v5

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+   {
+      name: 'Frog',
+      link: 'https://i.gifer.com/origin/6b/6b72817f6498f551062b2788084cb1f6_w200.gif'
+   },
+   {
+      name: 'Ricardo',
+      link: 'https://www.meme-arsenal.com/memes/c84127c51d392963f20772d51ed76ffd.jpg'
+   },
+   {
+      name: 'Клоун',
+      link: 'https://www.meme-arsenal.com/memes/308998085969483b920bf7bb77bf01f1.jpg'
+   },
+   {
+      name: 'Simpsons',
+      link: 'https://i.gifer.com/origin/ee/ee906a8fdddcf6a8f14d3faf63b1d078_w200.gif'
+   },
+   {
+      name: 'Koreyka',
+      link: 'https://media.tenor.com/K4pbXRYZmA8AAAAd/%D0%B0%D0%B1%D0%BE%D0%B1%D1%83%D1%81-%D0%B4%D0%B0%D1%88%D0%B0.gif'
+   },
+   {
+      name: 'KoronaTime',
+      link: 'https://s1.bloknot-krasnodar.ru/thumb/850x0xcut/upload/iblock/6af/4aumvukqnzwpv9ibeblhhjz6fbo419mm/b5b5db5b3f4507784922742775bd4dea.png'
+   }
+];
--- scripts/script.js
+++ scripts/script.js
@@ -1,73 +1,62 @@
 const popupRedact = document.querySelector(".popup_type_redact");
-const formElement = document.querySelector(".form")
+const formRedactProfile = document.querySelector(".form")
 const nameInput = document.querySelector(".form__input_type_name");
 const jobInput = document.querySelector(".form__input_type_job");
-const textInput = document.querySelector(".form__input_type_text");
+const nameInputCard = document.querySelector(".form__input_type_text");
 const linkInput = document.querySelector(".form__input_type_link");
 const profileName = document.querySelector(".profile__title");
 const profileJob = document.querySelector(".profile__subtitle");
 const openPopupButtonProfile = document.querySelector(".profile__edit-button");
 const addCardButton = document.querySelector(".profile__add-button");
 const popupCard = document.querySelector(".popup_type_card");
-const closePopupButton = document.querySelectorAll(".popup__close");
+const buttonsClosePopups = document.querySelectorAll(".popup__close");
 const cardsBlock = document.querySelector(".cards");
 const formCard = document.querySelector(".form_type_card");
 const popupImg = document.querySelector(".popup_type_img");
 const imgPopup = document.querySelector(".popup__img");
 const captionPopup = document.querySelector(".popup__caption");
-const initialCards = [
-   {
-      name: 'Frog',
-      link: 'https://i.gifer.com/origin/6b/6b72817f6498f551062b2788084cb1f6_w200.gif'
-   },
-   {
-      name: 'Ricardo',
-      link: 'https://www.meme-arsenal.com/memes/c84127c51d392963f20772d51ed76ffd.jpg'
-   },
-   {
-      name: 'Клоун',
-      link: 'https://www.meme-arsenal.com/memes/308998085969483b920bf7bb77bf01f1.jpg'
-   },
-   {
-      name: 'Simpsons',
-      link: 'https://i.gifer.com/origin/ee/ee906a8fdddcf6a8f14d3faf63b1d078_w200.gif'
-   },
-   {
-      name: 'Koreyka',
-      link: 'https://media.tenor.com/K4pbXRYZmA8AAAAd/%D0%B0%D0%B1%D0%BE%D0%B1%D1%83%D1%81-%D0%B4%D0%B0%D1%88%D0%B0.gif'
-   },
-   {
-      name: 'KoronaTime',
-      link: 'https://s1.bloknot-krasnodar.ru/thumb/850x0xcut/upload/iblock/6af/4aumvukqnzwpv9ibeblhhjz6fbo419mm/b5b5db5b3f4507784922742775bd4dea.png'
-   }
-];
-function createCard(name, link) {
-   const newCard = document.querySelector("#card").content.cloneNode(true);
-   newCard.querySelector(".card__img").src = link;
-   newCard.querySelector(".card__img").alt = name;
-   newCard.querySelector(".card__img").addEventListener("click", openModalImg);
-   newCard.querySelector(".card__title").textContent = name;
-   newCard.querySelector(".card__like").addEventListener("click", clickLikeCard);
+const cardsTemplate = document.querySelector("#card").content;
+
+function createCard(card) {
+   const newCard = cardsTemplate.cloneNode(true);
+   const newCardImage = newCard.querySelector(".card__img");
+   const newCardTitle = newCard.querySelector(".card__title");
+   newCardImage.src = card.link;
+   newCardImage.alt = card.name;
+   newCardImage.addEventListener("click", () => openModalImg(newCardImage, newCardTitle));
+   newCardTitle.textContent = card.name;
+   newCard.querySelector(".card__like").addEventListener("click", handleLikeClick);
    newCard.querySelector(".card__delite").addEventListener("click", deliteCard);
-   cardsBlock.prepend(newCard);
+   return newCard;
 };
-initialCards.forEach(item => { createCard(item.name, item.link) });
-function openModalImg(e) {
-   imgPopup.src = e.target.src
-   captionPopup.textContent = e.target.alt;
+function renderCard(item) {
+   cardsBlock.prepend(createCard(item));
+};
+initialCards.forEach(item => {
+   cardsBlock.prepend(createCard(item))
+});
+function openModalImg(link, name) {
+   imgPopup.src = link.src;
+   captionPopup.textContent = name.textContent;
+   imgPopup.alt = link.alt;
    openPopup(popupImg)
 };
 function deliteCard(e) {
    e.target.closest(".card").remove();
 };
-function clickLikeCard(e) {
+function handleLikeClick(e) {
    e.target.classList.toggle("card__like_active");
 };
-function handleFormSubmit(evt) {
+function handleFormSubmitRedactProfile(evt) {
    evt.preventDefault();
    profileName.textContent = nameInput.value;
    profileJob.textContent = jobInput.value;
    closePopup(popupRedact);
+};
+function openPopupEdit() {
+   nameInput.value = profileName.textContent;
+   jobInput.value = profileJob.textContent;
+   openPopup(popupRedact);
 };
 function openPopup(item) {
    item.classList.add("popup_opened");
@@ -75,12 +64,17 @@ function openPopup(item) {
 function closePopup(item) {
    item.classList.remove("popup_opened");
 };
-openPopupButtonProfile.addEventListener("click", function () {
-   nameInput.value = profileName.textContent;
-   jobInput.value = profileJob.textContent;
-   openPopup(popupRedact)
-});
-closePopupButton.forEach(item => {
+function addCard(e) {
+   e.preventDefault();
+   closePopup(popupCard);
+   renderCard({
+      name: nameInputCard.value,
+      link: linkInput.value,
+   });
+   formCard.reset();
+};
+openPopupButtonProfile.addEventListener("click", openPopupEdit);
+buttonsClosePopups.forEach(item => {
    item.addEventListener("click", function () {
       const element = item.closest(".popup");
       closePopup(element);
@@ -89,12 +83,5 @@ closePopupButton.forEach(item => {
 addCardButton.addEventListener("click", function () {
    openPopup(popupCard);
 });
-formElement.addEventListener('submit', handleFormSubmit);
-function addCard(e) {
-   e.preventDefault();
-   closePopup(popupCard);
-   createCard(textInput.value, linkInput.value);
-   textInput.value = "";
-   linkInput.value = "";
-};
+formRedactProfile.addEventListener('submit', handleFormSubmitRedactProfile);
 formCard.addEventListener("submit", addCard);
